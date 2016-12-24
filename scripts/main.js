242c79cbe060de0b586e265779c4bddba2daa824
Mohamed Qamhieh <qamhieh@harri.com>
2016-12-24 12:57:49 +0200

    Styling slider

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,10 @@
 /**
  * Created by Mohamed on 11/26/2016.
  */
+$(document).ready(function () {
+    $(".slider").slick({
+        dots: true,
+        prevArrow: '<i class="fa fa-chevron-left arrow-left"></i>',
+        nextArrow: '<i class="fa fa-chevron-right arrow-right"></i>'
+    });
+});
